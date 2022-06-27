a6c87cda9fbd940a7d34508a248ebc4dc5857e1d
wizard1066 <mark.lucking@gmail.com>
2022-06-27 10:27:21 +0200

    Initial Commit

---
+++ SceneA/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SceneA
+//
+//  Created by localuser on 27.06.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SceneA/SceneAApp.swift
@@ -0,0 +1,17 @@
+//
+//  SceneAApp.swift
+//  SceneA
+//
+//  Created by localuser on 27.06.22.
+//
+
+import SwiftUI
+
+@main
+struct SceneAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
